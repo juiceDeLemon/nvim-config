81ed4ac3801df1c1eb4cdb61dc5c5b3df6ad9e9e
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-10-06 19:27:49 +0800

    banned the word emacs and added autosave
    banned the word emacs because no emacs. added autosave in the form of autocommand

--- lua/plugin-settings/lsp/null-ls.lua
+++ lua/plugin-settings/lsp/null-ls.lua
@@ -5,27 +5,66 @@
 -- ██║░╚███║╚██████╔╝███████╗███████╗░░░░░░███████╗██████╔╝██╗███████╗╚██████╔╝██║░░██║
 -- ╚═╝░░╚══╝░╚═════╝░╚══════╝╚══════╝░░░░░░╚══════╝╚═════╝░╚═╝╚══════╝░╚═════╝░╚═╝░░╚═╝
 
-local null_status_ok, null_ls = pcall(require, "null-ls")
-if not null_status_ok then
-	return
-end
+local null_ls = require("null-ls")
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
+local null_format = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+local null_diag = null_ls.builtins.diagnostics
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
--- npm install --save-dev prettier prettier-plugin-solidity
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local no_emacs = {
+	method = null_ls.methods.DIAGNOSTICS,
+	filetypes = { "markdown", "text" },
+	generator = {
+		fn = function(params)
+			local diagnostics = {}
+			-- sources have access to a params object
+			-- containing info about the current file and editor state
+			for i, line in ipairs(params.content) do
+				local col, end_col = line:find("emacs")
+				if col and end_col then
+					-- null-ls fills in undefined positions
+					-- and converts source diagnostics into the required format
+					table.insert(diagnostics, {
+						row = i,
+						col = col,
+						end_col = end_col + 1,
+						source = "no-emacs",
+						message = 'Don\'t use "emacs"',
+						severity = vim.diagnostic.severity.HINT,
+					})
+				end
+			end
+			return diagnostics
+		end,
+	},
+}
+
 null_ls.setup({
 	autostart = true,
 	debug = false,
 	sources = {
-        formatting.eslint, -- js / ts / frameworks require plugins
-        formatting.autopep8, -- python
-		formatting.stylua, -- lua
-		formatting.google_java_format, -- java
-		formatting.clang_format, -- mainly c and cpp
-		diagnostics.flake8,
+		null_format.eslint, -- js / ts / frameworks require plugins
+		null_format.autopep8, -- python
+		null_format.stylua, -- lua
+		null_format.google_java_format, -- java
+		null_format.clang_format, -- mainly c and cpp
+		null_diag.flake8,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end -- autosave
+	end,
 })
+
+null_ls.register(no_emacs)
