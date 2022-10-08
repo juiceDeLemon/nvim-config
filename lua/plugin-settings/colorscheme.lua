9dd3b7b76ee51763f2790e10385158302c31899d
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-10-08 19:36:53 +0800

    neovide and term transparency
    formatted and changed the transparent option to fit neovide

--- lua/plugin-settings/colorscheme.lua
+++ lua/plugin-settings/colorscheme.lua
@@ -4,18 +4,13 @@
 -- ██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗██║░░██╗██╔══██║██╔══╝░░██║╚██╔╝██║██╔══╝░░░░░██║░░░░░██║░░░██║██╔══██║
 -- ╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝╚█████╔╝██║░░██║███████╗██║░╚═╝░██║███████╗██╗███████╗╚██████╔╝██║░░██║
 -- ░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚══════╝╚═╝╚══════╝░╚═════╝░╚═╝░░╚═╝
-
-local status_ok, onedark = pcall(require, "onedark")
-if not status_ok then
-  return
-end
-
-local colours = require("colours")
+local onedark = require "onedark"
+local colours = require "colours"
 
 onedark.setup({
     functionStyle = "underline",
     variableStyle = "bold",
-    transparent = false,
+    transparent = not require "neovide",
     customTelescope = true,
 })
 
@@ -24,18 +19,18 @@ onedark.setup({
 -- otherwise the setup function will override the custom settings
 local highlights = { -- TODO: hi link https://neovim.io/doc/user/api.html
     -- change search highlight background colour
-    IncSearch = {bg = colours.yellow, fg = colours.dark_text}, -- searching
-    Search = {bg = colours.comment, fg = colours.bg}, -- results
+    IncSearch = { bg = colours.yellow, fg = colours.dark_text }, -- searching
+    Search = { bg = colours.comment, fg = colours.bg }, -- results
     -- change split separator colour
-    VertSplit = {fg = colours.text},
+    VertSplit = { fg = colours.text },
     -- change cursorline and cursorcolumn highlight
-    CursorColumn = {bg = "#2D313B"},
-    CursorLine = {bg = "#2D313B"},
+    CursorColumn = { bg = "#2D313B" },
+    CursorLine = { bg = "#2D313B" },
     -- cmp
     -- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
     -- (use nvim treesitter highlight group as reference)
-    CmpItemAbbrMatch = {fg = colours.red, bold = true},
-    CmpItemMenu = {fg = colours.teal, italic = true}, -- [Lsp] tags
+    CmpItemAbbrMatch = { fg = colours.red, bold = true },
+    CmpItemMenu = { fg = colours.teal, italic = true }, -- [Lsp] tags
     -- https://vim.fandom.com/wiki/Identify_the_syntax_highlighting_group_used_at_the_cursor (cursor change colour)
     -- CmpItemKindFunction = {bg = colours.blue, fg = colours.bg},
     -- CmpItemKindVariable = {bg = colours.yellow, fg = colours.bg},
@@ -43,7 +38,4 @@ local highlights = { -- TODO: hi link https://neovim.io/doc/user/api.html
     -- CmpItemKindFile = {bg = colours.green, fg = colours.text},
 }
 
-for k, v in pairs(highlights) do
-    vim.api.nvim_set_hl(0, k, v)
-end
-
+for k, v in pairs(highlights) do vim.api.nvim_set_hl(0, k, v) end
