bfdbc34c192b9600b98537ee023ac921bc35e707
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-10-10 22:45:31 +0800

    british spelling (colourscheme) and no word_diff

--- lua/plugin-settings/gitsigns.lua
+++ lua/plugin-settings/gitsigns.lua
@@ -19,15 +19,10 @@ gs.setup {
             linehl = "GitSignsAddLn",
         },
     },
-    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-    word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
-    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 3000,
-    },
+    signcolumn = true,
+    numhl = true,
+    current_line_blame = true,
+    current_line_blame_opts = { virt_text = true, virt_text_pos = "eol", delay = 3000 },
     preview_config = {
         -- Options passed to nvim_open_win
         border = "rounded",
