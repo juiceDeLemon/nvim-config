f8753a559d955bd54ba86ecb2dac3eceb8007315
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-10-25 17:21:07 +0800

    minor refines
    formatted, removed safe call

--- lua/plugin-settings/navic.lua
+++ lua/plugin-settings/navic.lua
@@ -4,16 +4,8 @@
 -- ██║╚████║██╔══██║░╚████╔╝░██║██║░░██╗░░░██║░░░░░██║░░░██║██╔══██║
 -- ██║░╚███║██║░░██║░░╚██╔╝░░██║╚█████╔╝██╗███████╗╚██████╔╝██║░░██║
 -- ╚═╝░░╚══╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝╚══════╝░╚═════╝░╚═╝░░╚═╝
+local navic = require "nvim-navic"
 
-local status_ok, navic = pcall(require, "nvim-navic")
-if not status_ok then
-    return
-end
-
-navic.setup {
-    separator = "  ",
-    depth_limit = 4,
-}
+navic.setup { separator = "  ", depth_limit = 4 }
 
 vim.o.statusline = "%{%v:lua.require'nvim-navic'.get_location()%}"
-
