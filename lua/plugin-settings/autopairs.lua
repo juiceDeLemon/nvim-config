5e1c45dec5546093a0de7773068f740db53715af
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-10-29 12:09:58 +0800

    removed redundant options

--- lua/plugin-settings/autopairs.lua
+++ lua/plugin-settings/autopairs.lua
@@ -8,23 +8,8 @@ local autopairs = require "nvim-autopairs"
 
 autopairs.setup {
     check_ts = true,
-    ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-        java = false,
-    },
+    ts_config = { lua = { "string", "source" }, javascript = { "string", "template_string" } },
     disable_filetype = { "TelescopePrompt", "spectre_panel" },
-    fast_wrap = {
-        map = "<M-e>",
-        chars = { "{", "[", "(", "\"", "'" },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        offset = 0, -- Offset from pattern match
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-    },
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
