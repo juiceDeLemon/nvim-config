dd11327a33f09d93658ea4e834922d069adbbea8
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-11-07 22:06:40 +0800

    add clarification

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -16,6 +16,8 @@ vim.g.neovide_input_use_logo = true
 vim.g.neovide_cursor_animation_length = 0.1
 vim.g.neovide_cursor_trail_size = 0.1
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
+
+-- mac
 vim.api.nvim_set_keymap("", "<D-c>", "\"+y", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<D-v>", "\"+p", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("", "<D-x>", "\"+d", { noremap = true, silent = true })
