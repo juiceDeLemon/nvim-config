a66ed6819c10cf9fa9b37a52660c79b9df2e0143
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2022-11-27 23:56:16 +0800

    test file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,7 +12,7 @@ local opts = { noremap = true, silent = true }
 -- idk what this does
 -- local term_opts = { silent = true }
 
-local keymap = vim.keymap.set -- shorten keymap function name
+local keymap = vim.keymap.set
 
 ---------------
 -- REMINDERS --
@@ -64,12 +64,12 @@ keymap("x", ">", ">gv", opts)
 -- OTHER KEYMAPS (PLUGINS AND OTHERS) --
 ----------------------------------------
 -- dial
-keymap("n", "<C-a>", require("dial.map").inc_normal(), opts)
-keymap("n", "<C-x>", require("dial.map").dec_normal(), opts)
-keymap("v", "<C-a>", require("dial.map").inc_visual(), opts)
-keymap("v", "<C-x>", require("dial.map").dec_visual(), opts)
-keymap("v", "g<C-a>", require("dial.map").inc_gvisual(), opts)
-keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), opts)
+keymap("n", "<C-a>", require"dial.map".inc_normal(), opts)
+keymap("n", "<C-x>", require"dial.map".dec_normal(), opts)
+keymap("v", "<C-a>", require"dial.map".inc_visual(), opts)
+keymap("v", "<C-x>", require"dial.map".dec_visual(), opts)
+keymap("v", "g<C-a>", require"dial.map".inc_gvisual(), opts)
+keymap("v", "g<C-x>", require"dial.map".dec_gvisual(), opts)
 
 -- move.nvim
 keymap({ "n", "i" }, "<C-j>", "<cmd>MoveLine(1)<CR>", opts)
@@ -80,3 +80,7 @@ keymap("v", "<C-j>", "<cmd>MoveBlock(1)<CR>", opts)
 keymap("v", "<C-k>", "<cmd>MoveBlock(-1)<CR>", opts)
 keymap("v", "<C-h>", "<cmd>MoveHBlock(-1)<CR>", opts)
 keymap("v", "<C-l>", "<cmd>MoveHBlock(1)<CR>", opts)
+
+-- ufo
+keymap("n", "zR", require"ufo".openAllFolds, opts)
+keymap("n", "zM", require"ufo".closeAllFolds, opts)
