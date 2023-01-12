b0fdda46478d1071f8231447583b869d0d22aaf3
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-01-12 17:14:46 +0800

    some keymaps and keymap() -> map()

--- lua/u/keymaps.lua
+++ lua/u/keymaps.lua
@@ -1,16 +1,10 @@
-------------------------
--- COMMONLY USED CODE --
-------------------------
 local opts = { noremap = true, silent = true }
 
 -- idk what this does
 -- local term_opts = { silent = true }
 
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
----------------
--- REMINDERS --
----------------
 -- n = normal mode
 -- i = insert mode
 -- v = visual and select mode
@@ -19,41 +13,32 @@ local keymap = vim.keymap.set
 -- c = command-line mode
 -- o = operator pending mode
 
--- put character in the place of *
--- CTRL = <c-*> (alt = a, meta = m)
--- FUNCTION = <F*>
--- ESCAPE = <ESC>
--- RETURN(ENTER) = <cr>
--- SPACE = <space>
--- ENTER COMMAND = <cmd>
--- LEADER = <leader>
--- TAB = <tab> NOTE: you can't use modifier keys like ctrl, alt and meta
--- NO MAPPING = <nop>
-
--------------------------------
--- NON-MODE-SPECIFIC KEYMAPS --
--------------------------------
 -- space as leader key
-keymap("", "<space>", "<Nop>", opts) -- reset
+map("", "<space>", "<nop>", opts) -- reset
 vim.g.mapleader = " " -- map
 vim.g.maplocalleader = " " -- map
 
 -- resize splits with arrows
-keymap("", "<C-Up>", "<cmd>resize -2<cr>", opts)
-keymap("", "<C-Down>", "<cmd>resize +2<cr>", opts)
-keymap("", "<C-Left>", "<cmd>vertical resize -2<cr>", opts)
-keymap("", "<C-Right>", "<cmd>vertical resize +2<cr>", opts)
+map("", "<C-Up>", "<cmd>resize -2<cr>", opts)
+map("", "<C-Down>", "<cmd>resize +2<cr>", opts)
+map("", "<C-Left>", "<cmd>vertical resize -2<cr>", opts)
+map("", "<C-Right>", "<cmd>vertical resize +2<cr>", opts)
 
--------------------------
--- VISUAL MODE KEYMAPS --
--------------------------
 -- continue to select text went indenting
-keymap("x", "<", "<gv", opts)
-keymap("x", ">", ">gv", opts)
+map("x", "<", "<gv", opts)
+map("x", ">", ">gv", opts)
 
-----------------------------------------
--- OTHER KEYMAPS (PLUGINS AND OTHERS) --
-----------------------------------------
+-- replace paste without occupying paste register
+map("x", "p", '"_dP')
+
+-- system clipboard yanking
+map("n", "Y", '"+y')
+map("n", "<c-y>", '"+yy')
+map("n", "D", '"+d')
+map("n", "<c-d>", '"+dd')
+map("v", "Y", '"+y')
+map("v", "D", '"+d')
+
 -- dial
 -- keymap("n", "<C-a>", require"dial.map".inc_normal(), opts)
 -- keymap("n", "<C-x>", require"dial.map".dec_normal(), opts)
@@ -63,11 +48,11 @@ keymap("x", ">", ">gv", opts)
 -- keymap("v", "g<C-x>", require"dial.map".dec_gvisual(), opts)
 
 -- move.nvim
-keymap({ "n", "i" }, "<C-j>", "<cmd>MoveLine(1)<CR>", opts)
-keymap({ "n", "i" }, "<C-k>", "<cmd>MoveLine(-1)<CR>", opts)
-keymap({ "n", "i" }, "<C-h>", "<cmd>MoveHChar(-1)<CR>", opts)
-keymap({ "n", "i" }, "<C-l>", "<cmd>MoveHChar(1)<CR>", opts)
-keymap("v", "<C-j>", "<cmd>MoveBlock(1)<CR>", opts)
-keymap("v", "<C-k>", "<cmd>MoveBlock(-1)<CR>", opts)
-keymap("v", "<C-h>", "<cmd>MoveHBlock(-1)<CR>", opts)
-keymap("v", "<C-l>", "<cmd>MoveHBlock(1)<CR>", opts)
+map({ "n", "i" }, "<C-j>", "<cmd>MoveLine(1)<CR>", opts)
+map({ "n", "i" }, "<C-k>", "<cmd>MoveLine(-1)<CR>", opts)
+map({ "n", "i" }, "<C-h>", "<cmd>MoveHChar(-1)<CR>", opts)
+map({ "n", "i" }, "<C-l>", "<cmd>MoveHChar(1)<CR>", opts)
+map("v", "<C-j>", "<cmd>MoveBlock(1)<CR>", opts)
+map("v", "<C-k>", "<cmd>MoveBlock(-1)<CR>", opts)
+map("v", "<C-h>", "<cmd>MoveHBlock(-1)<CR>", opts)
+map("v", "<C-l>", "<cmd>MoveHBlock(1)<CR>", opts)
