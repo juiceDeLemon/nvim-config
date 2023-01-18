447a68fc01a8964bcb6b8d09c73c3771ff84f930
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-01-18 19:52:47 +0800

    locality improves cmp "smartness"

--- lua/u/pluginsettings/completion/init.lua
+++ lua/u/pluginsettings/completion/init.lua
@@ -79,6 +79,7 @@ cmp.setup {
             cmp.config.compare.exact,
             cmp.config.compare.score,
             require("cmp-under-comparator").under,
+            cmp.config.compare.locality,
             cmp.config.compare.kind,
             cmp.config.compare.sort_text,
             cmp.config.compare.length,
