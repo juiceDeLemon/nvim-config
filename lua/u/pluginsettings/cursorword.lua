f4d245bcbb2bc19fdd03b114969f105fd4486b5f
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-02-16 22:52:00 +0800

    cursorword moved here

---
+++ lua/u/pluginsettings/cursorword.lua
@@ -0,0 +1,14 @@
+_G.cursorword_blocklist = function()
+    local curword = vim.fn.expand "<cword>"
+    local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+
+    -- Add any disabling global or filetype-specific logic here
+    local blocklist = {}
+    if filetype == "lua" then blocklist = { "--", "---" } end
+
+    vim.b.minicursorword_disable = vim.tbl_contains(blocklist, curword)
+end
+
+-- Make sure to add this autocommand *before* calling module's `setup()`.
+vim.api.nvim_create_autocmd("CursorMoved", { pattern = "*", callback = function() _G.cursorword_blocklist() end })
+require("mini.cursorword").setup { delay = 600 }
