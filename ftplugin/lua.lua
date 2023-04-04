167df8f06e5f7d1533c6ddc7f815eedecf8bb569
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-04-04 12:36:16 +0800

    indent with 3 is the best, and added gf to lua

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,12 @@
-vim.bo.tabstop = 4
-vim.bo.softtabstop = 4
-vim.bo.shiftwidth = 4
+vim.bo.tabstop = 3
+vim.bo.softtabstop = 3
+vim.bo.shiftwidth = 3
+
+-- STOLEN FROM https://github.com/sam4llis/nvim-lua-gf/
+-- Options to add `gf` functionality inside `.lua` files.
+vim.opt_local.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
+vim.opt_local.includeexpr = "substitute(v:fname,'\\.','/','g')"
+for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
+   vim.opt_local.path:append(path .. "/lua")
+end
+vim.opt_local.suffixesadd:prepend ".lua"
