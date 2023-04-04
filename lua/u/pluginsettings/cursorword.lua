6a2429719a808defa33db8ac592a274e580cc0de
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-04-04 12:36:36 +0800

    block true and false

--- lua/u/pluginsettings/cursorword.lua
+++ lua/u/pluginsettings/cursorword.lua
@@ -4,7 +4,7 @@ _G.cursorword_blocklist = function()
 
     -- Add any disabling global or filetype-specific logic here
     local blocklist = {}
-    if filetype == "lua" then blocklist = { "--", "---" } end
+    if filetype == "lua" then blocklist = { "--", "---", "true", "false" } end
 
     vim.b.minicursorword_disable = vim.tbl_contains(blocklist, curword)
 end
