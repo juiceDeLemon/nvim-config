2e75283837291ff1a3bcbe631c4b0cbbe5b5c785
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-04-11 15:04:39 +0800

    formatting, -whichkey, navvbuddy attach, "pp" global, -annoying popup, lsplines: enable @ startup -> disabled @ startup and toggalable

--- lua/u/pluginsettings/lsp.lua
+++ lua/u/pluginsettings/lsp.lua
@@ -1,100 +1,104 @@
 require("mason").setup { ui = { border = "rounded" } }
 
 require("mason-lspconfig").setup {
-    ensure_installed = {
-        "bashls",
-        "jsonls",
-        "lua_ls",
-        "marksman",
-        "pylsp",
-    },
+   ensure_installed = {
+      "bashls",
+      "jsonls",
+      "lua_ls",
+      "marksman",
+      "pylsp",
+   },
 }
 
-local on_attach = function(_, _)
-    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { noremap = true, silent = true })
-    require("which-key").register({
-        d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Definition" },
-        D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Declaration" },
-        I = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Implementation" },
-        r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-    }, { mode = "n", prefix = "g" })
-    require("which-key").register({
-        l = {
-            name = "LSP",
-            a = { "<cmd>CodeActionMenu<cr>", "Code Action" },
-            h = { "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", "Previous" },
-            l = { "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", "Next" },
-            r = { ":IncRename ", "Rename Symbol" },
-            s = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
-            q = { "<cmd>TroubleToggle document_diagnostics<cr>", "Diagnostics" },
-        },
-    }, { mode = "n", prefix = "<leader>" })
+local on_attach = function(client, bufnr)
+   local map = vim.keymap.set
+   -- stylua: ignore start
+   map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { noremap = true, silent = true })
+   map("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", { noremap = true, silent = true, desc = "Definitions" })
+   map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", { noremap = true, silent = true, desc = "Declarations" })
+   map("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<cr>", { noremap = true, silent = true, desc = "Implementations" })
+   map("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { noremap = true, silent = true, desc = "References" })
+   map("n", "<leader>la", "<cmd>lua require('code_action_menu').open_code_action_menu()<cr>", { noremap = true, silent = true, desc = "Code Action" })
+   map("n", "<leader>lh", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", { noremap = true, silent = true, desc = "Previous" })
+   map("n", "<leader>ll", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", { noremap = true, silent = true, desc = "Next" })
+   map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { noremap = true, silent = true, desc = "Rename Symbol" })
+   map("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<cr>", { noremap = true, silent = true, desc = "Signature Help" })
+   map("n", "<leader>lt", "<cmd>lua require'lsp_lines'.toggle()<cr>", { noremap = true, silent = true, desc = "Toggle LspLines" })
+   map("n", "<leader>lq", "<cmd>TroubleToggle document_diagnostics<cr>", { noremap = true, silent = true, desc = "Diagnostics" })
+   -- stylua: ignore end
+   require("nvim-navbuddy").attach(client, bufnr)
 end
 
 require("neodev").setup {}
 
 require("mason-lspconfig").setup_handlers {
-    function(server_name)
-        require("lspconfig")[server_name].setup {
-            on_attach = on_attach,
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        }
-    end,
+   function(server_name)
+      require("lspconfig")[server_name].setup {
+         on_attach = on_attach,
+         capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      }
+   end,
 }
 require("lspconfig").lua_ls.setup {
-    settings = { Lua = { diagnostics = { globals = { "vim", "require" } } } },
-    on_attach = on_attach,
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+   settings = {
+      Lua = {
+         diagnostics = { globals = { "vim", "require", "pp" } },
+         workspace = { checkThirdParty = false }, -- remove that annoying popup https://github.com/neovim/nvim-lspconfig/issues/1700
+      },
+   },
+   on_attach = on_attach,
+   capabilities = require("cmp_nvim_lsp").default_capabilities(),
 }
 
 vim.g.python3_host_prog = "/usr/local/bin/python3"
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+   local hl = "DiagnosticSign" .. type
+   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
-vim.diagnostic.config { virtual_text = false }
-require("lsp_lines").setup()
+local ll = require "lsp_lines"
+ll.setup()
+ll.toggle()
 
-local settings = {
-    pylsp = {
-        pylsp = {
-            builtin = {
-                installExtraArgs = {
-                    "flake8",
-                    "pycodestyle",
-                    "pydocstyle",
-                    "pyflakes",
-                    "pylint",
-                    "yapf",
-                },
-            },
-            plugins = {
-                jedi_completion = { enabled = false },
-                rope_completion = { enabled = false },
-                flake8 = { enabled = false },
-                pyflakes = { enabled = false },
-                pycodestyle = {
-                    ignore = {
-                        "E226",
-                        "E266",
-                        "E302",
-                        "E303",
-                        "E304",
-                        "E305",
-                        "E402",
-                        "C0103",
-                        "W0104",
-                        "W0621",
-                        "W391",
-                        "W503",
-                        "W504",
-                    },
-                    maxLineLength = 99,
-                },
-            },
-        },
-    },
-}
+-- local settings = {
+--     pylsp = {
+--         pylsp = {
+--             builtin = {
+--                 installExtraArgs = {
+--                     "flake8",
+--                     "pycodestyle",
+--                     "pydocstyle",
+--                     "pyflakes",
+--                     "pylint",
+--                     "yapf",
+--                 },
+--             },
+--             plugins = {
+--                 jedi_completion = { enabled = false },
+--                 rope_completion = { enabled = false },
+--                 flake8 = { enabled = false },
+--                 pyflakes = { enabled = false },
+--                 pycodestyle = {
+--                     ignore = {
+--                         "E226",
+--                         "E266",
+--                         "E302",
+--                         "E303",
+--                         "E304",
+--                         "E305",
+--                         "E402",
+--                         "C0103",
+--                         "W0104",
+--                         "W0621",
+--                         "W391",
+--                         "W503",
+--                         "W504",
+--                     },
+--                     maxLineLength = 99,
+--                 },
+--             },
+--         },
+--     },
+-- }
