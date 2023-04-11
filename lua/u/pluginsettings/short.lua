12630694d47f843e08859971a512754b4a1cb103
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-04-11 15:42:27 +0800

    -removed plugins, navbuddy

--- lua/u/pluginsettings/short.lua
+++ lua/u/pluginsettings/short.lua
@@ -13,12 +13,6 @@ require("hlslens").setup()
 -- git conflict
 require("git-conflict").setup {}
 
--- winsep
-require("colorful-winsep").setup()
-
--- animate
-require("mini.animate").setup { cursor = { enable = false } }
-
 -- todo comments
 require("todo-comments").setup { highlight = { before = "fg", keyword = "bg" } }
 
@@ -26,7 +20,7 @@ require("todo-comments").setup { highlight = { before = "fg", keyword = "bg" } }
 require("persisted").setup { use_git_branch = true, autosave = false }
 
 -- navbuddy
-require("nvim-navbuddy").setup {}
+require("nvim-navbuddy").setup()
 
 -- flight
 require("flight").setup()
