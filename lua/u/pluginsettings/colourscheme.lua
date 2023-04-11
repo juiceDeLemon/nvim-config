1c21381700f5749753aa0a7c74be52b25ee0265f
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-04-11 15:07:31 +0800

    formatting, remove useless

--- lua/u/pluginsettings/colourscheme.lua
+++ lua/u/pluginsettings/colourscheme.lua
@@ -1,26 +1,24 @@
 require("tokyonight").setup {
-    style = "night",
-    styles = { functions = { bold = true }, sidebars = "transparent", floats = "transparent" },
-    transparent = true,
-    dim_inactive = true,
-    lualine_bold = true,
-    on_highlights = function(hi, c)
-        -- colours refers to here:
-        -- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
-        hi.TreesitterContextBottom = { bold = true, underline = true }
-        hi.CursorLineNr = { fg = c.fg }
-        hi.LineNr = { fg = c.dark5 }
-        hi.Pmenu = { fg = c.border_highlight }
-        hi.IndentBlanklineSpaceChar = { fg = c.Whitespace }
-        hi.CmpItemAbbrMatch = { fg = c.blue1, bold = true }
-        hi.CmpItemMenu = { fg = c.comment, italic = true }
-        hi.Visual = { bold = true, reverse = true }
-        hi.TelescopePreviewLine = { bg = c.bg_visual }
-        hi.TelescopeSelection = { bg = c.bg_visual }
-        hi.TelescopeSelectionCaret = { bg = c.bg_visual }
-        hi.EndOfBuffer = { fg = c.comment }
-        hi.WinSeparator = { fg = c.comment }
-    end,
+   style = "night",
+   styles = { functions = { bold = true }, sidebars = "transparent", floats = "transparent" },
+   transparent = true,
+   dim_inactive = true,
+   lualine_bold = true,
+   on_highlights = function(hi, c)
+      -- colours refers to here:
+      -- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
+      hi.TreesitterContextBottom = { bold = true, underline = true }
+      hi.CursorLineNr = { fg = c.fg }
+      hi.LineNr = { fg = c.dark5 }
+      hi.Pmenu = { fg = c.border_highlight }
+      hi.CmpItemAbbrMatch = { fg = c.blue1, bold = true }
+      hi.CmpItemMenu = { fg = c.comment, italic = true }
+      hi.Visual = { bold = true, reverse = true }
+      hi.TelescopePreviewLine = { bg = c.bg_visual }
+      hi.TelescopeSelection = { bg = c.bg_visual }
+      hi.TelescopeSelectionCaret = { bg = c.bg_visual }
+      hi.EndOfBuffer = { fg = c.comment }
+   end,
 }
 
 vim.cmd.colorscheme "tokyonight"
