0d549005dbb37b0252fdcac928bf900953014bd9
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-05-02 00:00:55 +0800

    use <leader>lf to format instead of format on save

--- lua/u/pluginsettings/nullls.lua
+++ lua/u/pluginsettings/nullls.lua
@@ -28,13 +28,13 @@ nl.setup {
    },
    on_attach = function(client, bufnr)
       if client.supports_method "textDocument/formatting" then
-         vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-         vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function() lsp_formatting(bufnr) end,
-         })
-      end -- autosave
+         vim.keymap.set(
+            "n",
+            "<leader>lf",
+            function() lsp_formatting(bufnr) end,
+            { noremap = true, silent = true, desc = "Format File" }
+         )
+      end
    end,
 }
 
