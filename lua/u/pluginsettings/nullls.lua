374b549097d7defcf7bf95e47870ca8ad1653657
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-05-22 17:43:21 +0800

    luacheck redundant, removed no emacs rule, pylint with arguments

--- lua/u/pluginsettings/nullls.lua
+++ lua/u/pluginsettings/nullls.lua
@@ -12,11 +12,13 @@ local lsp_formatting = function(bufnr)
 end
 
 nl.setup {
-   autostart = true,
+   border = "rounded",
    debug = false,
    sources = {
-      d.luacheck,
-      d.pylint,
+      d.pylint.with { extra_args = {
+         "--ignored-modules=pygame",
+         "--good-names=x,y,z,i,j,n,f",
+      } },
       d.shellcheck,
 
       f.black,
@@ -35,33 +37,3 @@ nl.setup {
       end
    end,
 }
-
-local no_emacs = {
-   method = nl.methods.DIAGNOSTICS,
-   filetypes = { "markdown", "text" },
-   generator = {
-      fn = function(params)
-         local diagnostics = {}
-         -- sources have access to a params object
-         -- containing info about the current file and editor state
-         for i, line in ipairs(params.content) do
-            local col, end_col = line:find "emacs"
-            if col and end_col then
-               -- null-ls fills in undefined positions
-               -- and converts source diagnostics into the required format
-               table.insert(diagnostics, {
-                  row = i,
-                  col = col,
-                  end_col = end_col + 1,
-                  source = "no-emacs",
-                  message = 'Don\'t use "emacs"',
-                  severity = vim.diagnostic.severity.HINT,
-               })
-            end
-         end
-         return diagnostics
-      end,
-   },
-}
-
-nl.register(no_emacs)
