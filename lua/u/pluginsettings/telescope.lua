bcc7fdd23641ad72ab812f84a968268f2d3a5bb9
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-05-24 07:14:38 +0800

    there is no frecency

--- lua/u/pluginsettings/telescope.lua
+++ lua/u/pluginsettings/telescope.lua
@@ -14,7 +14,6 @@ require("telescope").setup {
    extensions = { lazy = { show_icon = false } },
 }
 
--- require("telescope").load_extension "frecency"
 require("telescope").load_extension "fzf"
 require("telescope").load_extension "lazy"
 require("telescope").load_extension "refactoring"
