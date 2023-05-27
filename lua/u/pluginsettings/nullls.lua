f59761c5c4b727163ab50d5679102652dc451acd
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-05-27 15:14:25 +0800

    formatting moved to lsp.lua

--- lua/u/pluginsettings/nullls.lua
+++ lua/u/pluginsettings/nullls.lua
@@ -3,13 +3,6 @@ local nl = require "null-ls"
 local d = nl.builtins.diagnostics
 local f = nl.builtins.formatting
 
-local lsp_formatting = function(bufnr)
-   vim.lsp.buf.format {
-      filter = function(client) return client.name == "null-ls" end,
-      bufnr = bufnr,
-   }
-end
-
 nl.setup {
    border = "rounded",
    debug = false,
@@ -26,14 +19,4 @@ nl.setup {
       f.prettier, -- json
       f.shfmt,
    },
-   on_attach = function(client, bufnr)
-      if client.supports_method "textDocument/formatting" then
-         vim.keymap.set(
-            "n",
-            "<leader>lf",
-            function() lsp_formatting(bufnr) end,
-            { noremap = true, silent = true, desc = "Format File" }
-         )
-      end
-   end,
 }
