be5aedbd4f67c08409717be5cdfe335b48b34f4a
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-06-02 19:56:45 +0800

    no yadm, sidebars icons changed to primitive +-=~

--- lua/u/pluginsettings/gitsigns.lua
+++ lua/u/pluginsettings/gitsigns.lua
@@ -1,22 +1,15 @@
 require("gitsigns").setup {
    signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = {
-         hl = "GitSignsAdd",
-         text = "▎",
-         numhl = "GitSignsAddNr",
-         linehl = "GitSignsAddLn",
-      },
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "-" },
+      -- topdelete = { text = "" },
+      changedelete = { text = "=" },
    },
    signcolumn = true,
    preview_config = {
-      -- Options passed to nvim_open_win
       border = "rounded",
       style = "minimal",
       relative = "cursor",
    },
-   yadm = { enable = true },
 }
