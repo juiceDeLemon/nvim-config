b7fb3cb5ac77c267735e2cf3c3df8ba4dc6baff0
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-06-13 18:00:47 +0800

    formatted the spaces in stylua ignore blocks

--- lua/u/pluginsettings/lsp.lua
+++ lua/u/pluginsettings/lsp.lua
@@ -6,21 +6,21 @@ require("mason-lspconfig").setup {
 
 local on_attach = function(client, bufnr)
 	local map = vim.keymap.set
-   -- stylua: ignore start
-   map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { noremap = true, silent = true })
-   map("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", { noremap = true, silent = true, desc = "Definitions" })
-   map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", { noremap = true, silent = true, desc = "Declarations" })
-   map("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<cr>", { noremap = true, silent = true, desc = "Implementations" })
-   map("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { noremap = true, silent = true, desc = "References" })
-   map("n", "<leader>la", "<cmd>lua require('code_action_menu').open_code_action_menu()<cr>", { noremap = true, silent = true, desc = "Code Action" })
-   map("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format {filter = function(client) return client.name == 'null-ls' end }<cr>", { noremap = true, silent = true, desc = "Format Code" })
-   map({ "n", "v", "o" }, "<leader>lh", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", { noremap = true, silent = true, desc = "Previous" })
-   map({ "n", "v", "o" }, "<leader>ll", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", { noremap = true, silent = true, desc = "Next" })
-   map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { noremap = true, silent = true, desc = "Rename Symbol" })
-   map("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<cr>", { noremap = true, silent = true, desc = "Signature Help" })
-   map({ "i", "s" }, "<c-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", { noremap = true, silent = true, desc = "Signature Help" })
-   map("n", "<leader>lt", "<cmd>lua require'lsp_lines'.toggle()<cr>", { noremap = true, silent = true, desc = "Toggle LspLines" })
-   map("n", "<leader>lq", "<cmd>TroubleToggle document_diagnostics<cr>", { noremap = true, silent = true, desc = "Diagnostics" })
+	-- stylua: ignore start
+	map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", { noremap = true, silent = true })
+	map("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", { noremap = true, silent = true, desc = "Definitions" })
+	map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", { noremap = true, silent = true, desc = "Declarations" })
+	map("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<cr>", { noremap = true, silent = true, desc = "Implementations" })
+	map("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { noremap = true, silent = true, desc = "References" })
+	map("n", "<leader>la", "<cmd>lua require('code_action_menu').open_code_action_menu()<cr>", { noremap = true, silent = true, desc = "Code Action" })
+	map("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format {filter = function(client) return client.name == 'null-ls' end }<cr>", { noremap = true, silent = true, desc = "Format Code" })
+	map({ "n", "v", "o" }, "<leader>lh", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", { noremap = true, silent = true, desc = "Previous" })
+	map({ "n", "v", "o" }, "<leader>ll", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", { noremap = true, silent = true, desc = "Next" })
+	map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { noremap = true, silent = true, desc = "Rename Symbol" })
+	map("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<cr>", { noremap = true, silent = true, desc = "Signature Help" })
+	map({ "i", "s" }, "<c-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", { noremap = true, silent = true, desc = "Signature Help" })
+	map("n", "<leader>lt", "<cmd>lua require'lsp_lines'.toggle()<cr>", { noremap = true, silent = true, desc = "Toggle LspLines" })
+	map("n", "<leader>lq", "<cmd>TroubleToggle document_diagnostics<cr>", { noremap = true, silent = true, desc = "Diagnostics" })
 	-- stylua: ignore end
 	require("nvim-navic").attach(client, bufnr)
 	require("nvim-navbuddy").attach(client, bufnr)
@@ -63,6 +63,7 @@ require("lspconfig").pylsp.setup {
 	settings = {
 		pylsp = {
 			plugins = {
+				jedi_completion = { enabled = true },
 				pyflakes = { enabled = false },
 				pycodestyle = {
 					ignore = {
