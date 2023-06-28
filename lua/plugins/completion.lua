0a8c5ce9f75fffbe0723e4682991de55cf42d4dd
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-06-28 19:19:05 +0800

    no cmp-under-comparator, use the extracted function

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -74,8 +74,20 @@ return {
 					comparators = {
 						cmp.config.compare.offset,
 						cmp.config.compare.exact,
+						cmp.config.compare.recently_used,
 						cmp.config.compare.score,
-						require("cmp-under-comparator").under,
+						-- stolen from tj's config, which is stolen from cmp-under-comparator
+						function(entry1, entry2)
+							local _, entry1_under = entry1.completion_item.label:find "^_+"
+							local _, entry2_under = entry2.completion_item.label:find "^_+"
+							entry1_under = entry1_under or 0
+							entry2_under = entry2_under or 0
+							if entry1_under > entry2_under then
+								return false
+							elseif entry1_under < entry2_under then
+								return true
+							end
+						end,
 						cmp.config.compare.locality,
 						cmp.config.compare.kind,
 						cmp.config.compare.sort_text,
@@ -128,7 +140,6 @@ return {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"lukas-reineke/cmp-under-comparator",
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
 		},
