587aacf41e9821d96e7d6d46771cf83f1e622b62
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-07-09 23:58:41 +0800

    forgot the formatting on markdowns

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,7 @@
 vim.keymap.set("n", "<leader><leader>p", vim.cmd.MarkdownPreviewToggle, { desc = "Toggle Markdown Preview" })
 vim.keymap.set("n", "<leader>a", vim.cmd.GenerateMarkdownTOC, { desc = "Generate Markdown Table of Context" })
+
+vim.b.lsp_format_activate = false
+vim.keymap.set("n", "<leader>lf", function()
+	vim.cmd "!prettier %"
+end, { noremap = true, desc = "Format File" })
