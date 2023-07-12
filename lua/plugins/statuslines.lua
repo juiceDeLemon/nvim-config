78bdb41408e3ee847f957e9168c157738d781798
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-07-12 23:26:45 +0800

    i don't need the statuscol

--- lua/plugins/statuslines.lua
+++ lua/plugins/statuslines.lua
@@ -9,38 +9,4 @@ return {
 		},
 		event = "VeryLazy",
 	},
-	{
-		"luukvbaal/statuscol.nvim",
-		config = function()
-			local builtin = require "statuscol.builtin"
-			require("statuscol").setup {
-				bt_ignore = { "terminal", "nofile" },
-				segments = {
-					{
-						sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 3, auto = true },
-					},
-					{
-						text = { builtin.lnumfunc },
-					},
-					{
-						sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, wrap = true, auto = true },
-					},
-					{
-						sign = { name = { "Dap" } },
-						maxwidth = 1,
-						colwidth = 1,
-						auto = true,
-					},
-					{
-						sign = { name = { "GitSigns" }, maxwidth = 2, colwidth = 1, auto = true },
-					},
-					{
-						text = { " " },
-					},
-				},
-			}
-		end,
-		dependencies = { "mfussenegger/nvim-dap", "lewis6991/gitsigns.nvim" },
-		event = "VeryLazy",
-	},
 }
