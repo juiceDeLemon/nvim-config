75a87884e45c3bb76286e9786b51f82e287e0844
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-07-31 23:51:13 +0800

    some user_commands that "fix" fast fingers when quitting neovim

--- lua/juicedelemon/modules/globals.lua
+++ lua/juicedelemon/modules/globals.lua
@@ -1,4 +1,6 @@
-pp = function(v)
-	vim.print(v)
-	return v
-end
+pp = vim.print
+
+-- fast fingers
+vim.api.nvim_create_user_command("Q", function() vim.cmd.q() end, {})
+vim.api.nvim_create_user_command("Qw", function() vim.cmd.wq() end, {})
+vim.api.nvim_create_user_command("Wq", function() vim.cmd.wq() end, {})
