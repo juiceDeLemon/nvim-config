2e167c90dd558fee2e0a7f6e01bf08354351d4c2
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-08-07 23:11:44 +0800

    more fast fingers to fix fast fingers

--- lua/juicedelemon/modules/globals.lua
+++ lua/juicedelemon/modules/globals.lua
@@ -4,3 +4,4 @@ pp = vim.print
 vim.api.nvim_create_user_command("Q", function() vim.cmd.q() end, {})
 vim.api.nvim_create_user_command("Qw", function() vim.cmd.wq() end, {})
 vim.api.nvim_create_user_command("Wq", function() vim.cmd.wq() end, {})
+vim.api.nvim_create_user_command("W", function() vim.cmd.w() end, {})
