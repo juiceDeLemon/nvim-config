1b4733840d2554d552f6fd37dbf33e0c1605baf5
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-08-31 19:27:21 +0800

    <c-x><c-n> ftw

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -46,7 +46,6 @@ return {
 				sources = cmp.config.sources {
 					{ name = "luasnip", group_index = 1 },
 					{ name = "nvim_lsp", group_index = 2 },
-					{ name = "buffer", group_index = 4, keyword_length = 5 },
 				},
 				sorting = {
 					comparators = {
